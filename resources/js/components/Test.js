c7e2409aa48e4186c81d2f5ea672a20977194a02
sonhyeinn <cannalily216@nsmg21.com>
2019-06-03 19:28:55 +0900

    docker update test 190603

---
+++ resources/js/components/Test.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class Test extends Component {
+    render() {
+        return (
+            <div>
+             dfkdfkdkf
+            </div>
+        );
+    }
+}
+
+export default Test;
+ReactDOM.render(<Test/>, document.getElementById('bar_react'));
---
+++ resources/views/test.blade.php
@@ -0,0 +1,7 @@
+<html>
+<head>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
+<body> <div id="bar_react"></div> </body>
+</html>
